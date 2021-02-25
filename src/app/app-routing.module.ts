4d72ed434dbd595e0381be5bdfad4c08b8f634fc
reshmamu <reshmaunnikrishnanm@gmail.com>
2021-02-25 12:13:12 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { HomeComponent } from './home/home.component';
+import { Home1Component } from './home1/home1.component';
+
+const routes: Routes = [
+
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+  path: "home1",
+  component: Home1Component
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Home1Component } from './home1/home1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    Home1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
